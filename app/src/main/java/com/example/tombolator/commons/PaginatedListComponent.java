816b6e93445ea08fbe2fc631eebcf3d492b25ac5
Mirco <mirco00riemer@aol.com>
2023-11-15 22:34:07 +0100

    Implemented pagination and page management into new pagination component. Sorting and filtering have to ne implemented and then component can be tested on tombolas list.

--- app/src/main/java/com/example/tombolator/commons/PaginatedListComponent.java
+++ app/src/main/java/com/example/tombolator/commons/PaginatedListComponent.java
@@ -1,6 +1,7 @@
 package com.example.tombolator.commons;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,18 +12,30 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import com.example.tombolator.R;
+import com.example.tombolator.media.MediaUtil;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
+/* TODO: Implement Sorting */
+/* TODO: Implement Filtering */
 public class PaginatedListComponent<T> extends ConstraintLayout {
 
-    private final MutableLiveData<List<T>> itemsLiveData = new MutableLiveData<>(new ArrayList<>());
+    private static final int ELEMENTS_PER_PAGE = 6;
+
+    private final MutableLiveData<Integer> currentPage = new MutableLiveData<>(1);
 
     private LifecycleOwner lifecycleOwner;
+
+    private LiveData<List<T>> items;
+
+    private Function<T, String> itemToStringConverter;
+    private OnClickListener itemOnClickListener;
 
     private ImageView sortButton;
 
@@ -81,5 +94,117 @@ public class PaginatedListComponent<T> extends ConstraintLayout {
         nextPageButton = innerView.findViewById(R.id.button_next_page);
 
         this.addView(innerView);
+
+        registerButtonListener();
+    }
+
+    private void setItems(LifecycleOwner lifecycleOwner, LiveData<List<T>> items) {
+
+        this.lifecycleOwner = lifecycleOwner;
+        this.items = items;
+
+        registerObserver(this.lifecycleOwner, this.items);
+    }
+
+    private void registerObserver(LifecycleOwner lifecycleOwner, LiveData<List<T>> items) {
+
+        items.observe(lifecycleOwner, this::showMediaOnCurrentPage);
+
+        currentPage.observe(lifecycleOwner, new PageNumberCurrentObserver(items));
+    }
+
+    private void registerButtonListener() {
+
+        nextPageButton.setOnClickListener((View view) -> {
+
+            if (currentPage.getValue() == null)
+                throw new IllegalStateException("Current page field should never be null!");
+
+            if (items.getValue() == null)
+                return;
+
+            if (currentPage.getValue() == MediaUtil.getTotalNumberOfPages(items.getValue(), ELEMENTS_PER_PAGE))
+                return;
+
+            currentPage.setValue(currentPage.getValue() + 1);
+        });
+
+        previousPageButton.setOnClickListener((View view) -> {
+
+            if (currentPage.getValue() == null)
+                throw new IllegalStateException("Current page field should never be null!");
+
+            if (currentPage.getValue() == 1)
+                return;
+
+            currentPage.setValue(currentPage.getValue() - 1);
+        });
+    }
+
+    private void showMediaOnCurrentPage(List<T> items) {
+
+        if (currentPage.getValue() == null)
+            throw new IllegalStateException("Current page field should never be null!");
+
+        int start = (currentPage.getValue() - 1) * ELEMENTS_PER_PAGE;
+        int end = start + ELEMENTS_PER_PAGE;
+
+        if (end > items.size())
+            end = items.size();
+
+        itemList.removeAllViews();
+
+        for (int i = start; i < end; i++) {
+
+            T item = items.get(i);
+
+            TextView textView = (TextView) View.inflate(getContext(), R.layout.list_element, null);
+
+            String text = itemToStringConverter == null ? item.toString() : itemToStringConverter.apply(item);
+
+            Typeface defaultTypeface = textView.getTypeface();
+            int defaultTextColor = textView.getCurrentTextColor();
+
+            /* TODO: Implement here possibility to mark items that are used for anything. */
+//            for (Media mediaInTombola : tombolasActivityViewModel.getSelectedTombola().getValue().getAllMedia()) {
+//
+//                if (id == mediaInTombola.getId()) {
+//
+//                    textView.setTextColor(Color.parseColor("#3700B3"));
+//                    textView.setTypeface(defaultTypeface, Typeface.BOLD);
+//                }
+//            }
+
+            textView.setText(text);
+
+            if (itemOnClickListener != null)
+                textView.setOnClickListener(itemOnClickListener);
+
+            itemList.addView(textView);
+        }
+    }
+
+    private void setItemToStringConverter(Function<T, String> itemToStringConverter) {
+        this.itemToStringConverter = itemToStringConverter;
+    }
+
+    private void setItemOnClickListener(OnClickListener itemOnClickListener) {
+        this.itemOnClickListener = itemOnClickListener;
+    }
+
+    private class PageNumberCurrentObserver implements Observer<Integer> {
+
+        private final LiveData<List<T>> items;
+
+        public PageNumberCurrentObserver(LiveData<List<T>> items) {
+            this.items = items;
+        }
+
+        @Override
+        public void onChanged(Integer pageNumber) {
+
+            currentPageLabel.setText(NumberUtil.formatNumberFullDigitsLeadingZero(pageNumber));
+            showMediaOnCurrentPage(items.getValue());
+        }
     }
 }
